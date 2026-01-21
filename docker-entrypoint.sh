2c380d3221833a3a8d785115f139246f657e1a79
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2026-01-21 15:31:22 +0700

    fix: comment out proxy_pass when BACKEND_URL not set
    - Allows nginx to start even without backend service
    - Prevents deployment failure when BACKEND_URL env var is missing
    - User must set BACKEND_URL in Coolify for API to work

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,13 @@
 set -e
 
 # If BACKEND_URL is set, substitute it in nginx config
+# If not set, comment out the proxy_pass so nginx can start (API won't work)
 if [ -n "$BACKEND_URL" ]; then
     echo "Configuring nginx to proxy to: $BACKEND_URL"
     sed -i "s|http://backend:8000|${BACKEND_URL}|g" /etc/nginx/nginx.conf
+else
+    echo "WARNING: BACKEND_URL not set - API proxy will be disabled"
+    sed -i "s|proxy_pass http://backend:8000;|#proxy_pass http://backend:8000; # BACKEND_URL not set|g" /etc/nginx/nginx.conf
 fi
 
 # Test nginx config
